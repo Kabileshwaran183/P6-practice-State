18ba8356419d0e442e6e7f04a8c7c90642717dc4
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2024-12-16 19:36:44 +0530

    complexState - Objects
    changing the empty fav button to star filled button

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import { createRoot } from 'react-dom/client'
 import App1 from './App1.jsx'
 import App2 from './state.jsx'
 import App3 from './complexState.jsx'
+import App4 from './complexStateObject.jsx'
+
 createRoot(document.getElementById('root')).render(
 
-    <App3 />
+    <App4 />
 
 )
